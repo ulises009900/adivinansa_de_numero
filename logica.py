c9bb6593fd3b1d24bb308445d2c0508017200309
lescanomauro <lescanomauro244@gmail.com>
2024-06-10 22:42:28 -0300

    edicion de mensajes

--- logica.py
+++ logica.py
@@ -1,13 +1,16 @@
 import random
 numero_secreto = random.randint(1, 100)
+print("bienvenido al juego de a divinar el numero!")
+print("Estoy pensando en un numero enter 1 y 100...")
+print ("¿puedes adivinar cual es?")
 
-eleccion=int(input("adivine el numero: "))
+eleccion=int(input("por favor ingrese el numero adivinar: "))
 while (eleccion!=numero_secreto):
     if (eleccion >numero_secreto):
-        eleccion=int(input("menos: "))
+        eleccion=int(input("demasiado bajo. Intente ingresar un nuevo numero: "))
     else:
-        eleccion=int(input("mas: "))
+        eleccion=int(input("demasiado alto. Intente ingresar un nuevo numero: "))
         
         
         
-print(eleccion, " es correcto")
+print(eleccion, "FELICITACIONES! has adivinado el numero")
